30c37597351745f1013d1b0bf73842807f99ec8f
juho.alasalmi <juho.alasalmi@ptt.fi>
2020-09-10 09:36:24 +0200

    Add data.

---
+++ R/data_kunta_1001.R
@@ -0,0 +1,11 @@
+#' Avoimet työpaikat, työttömät ja työvoima kunnittain
+#'
+#'
+#' @format A data frame
+#' \describe{
+#'   \item{Avoimet_tyopaikat}{}
+#'   \item{Tyovoima}{}
+#'   \item{Tyottomat}{}
+#' }
+#' @source \url{http://pxnet2.stat.fi/PXWeb/api/v1/fi/StatFin/tym/tyonv/kk/statfin_tyonv_pxt_1001.px}
+"data_kunta_1001"
---
+++ data-raw/tyonvalitystilasto_1001.R
@@ -0,0 +1,25 @@
+## code to prepare `DATASET` dataset goes here
+
+url <- "http://pxnet2.stat.fi/PXWeb/api/v1/fi/StatFin/tym/tyonv/kk/statfin_tyonv_pxt_1001.px"
+
+query <-
+  list("Alue"=c("*"),
+       "Kuukausi"= c("*"),
+       "Tiedot"=c("TYOTTOMAT","TYOVOIMA","AVPAIKAT"))
+
+# Hae data
+
+pxd <- pxweb::pxweb_get(url, query)
+data <- as.data.frame(pxd)
+
+data <- data %>%
+  statfitools::clean_names() %>%
+  mutate(Vuosi = substring(Kuukausi, 1,4),
+         Kuukausi = substring(Kuukausi, 5)) %>%
+  statfitools::clean_times(sub_year_col = "Kuukausi")
+
+data_kunta_1001 <- filter_region(data, "kunta")
+data_seutukunta_1001 <- filter_region(data, "seutukunta")
+data_maakunta_1001 <- filter_region(data, "maakunta")
+
+usethis::use_data(data_kunta_1001, data_seutukunta_1001, data_maakunta_1001, overwrite = TRUE)
