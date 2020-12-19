48286a98cb2f9235c9ac468a5973143dbb4e7807
juho.alasalmi <juho.alasalmi@ptt.fi>
2020-12-19 13:43:39 +0200

    kayta statfitools extract_name ja extract_code funktioita

--- R/filter_region.R
+++ R/filter_region.R
@@ -1,5 +1,13 @@
 #' Filters the desired region from the dataset
 #'
+#' #' Tilastokeskuksen taulukoissa on usein tietoja eri aggregointitasolla. Koska suurimmalla
+#' aggregointitasolla on vähiten suojattuja tietoja (vähemmän kuin 5 havaintoa) tulisi anlyysissä
+#' käyttää aina suurinta mahdollista aggregointitasoa siten että, jos haluaa aggregoida alueita,
+#' joita pystyy aggregoimaan seutukunnista, ei tulisi aggregoida kuntatiedoista, vaan seutukunta
+#' tiedoista. Jos aggregointiin voi käyttää maakuntia, vielä parempi.
+#'
+#' Kunnille ei tule koodeja suoraan pxwebin aineistosta
+#'
 #' @param data data.frame, contains the regional data
 #' @param region character, the desired region.
 #' @return data.frame
@@ -10,18 +18,18 @@ filter_region <- function(data, region) {
   if(region == "kunta") {
     output <- filter(data,!grepl("MK", Alue) & !grepl("SK", Alue) &
                        !grepl("ELY", Alue) & !grepl("KOKO", Alue)) %>%
-      mutate(kunta = Alue) %>%
+      mutate(kunta_name = Alue) %>%
       select(-Alue)
   } else if (region == "seutukunta") {
     output <- filter(data, grepl("SK", Alue))  %>%
-              mutate(seutukunta_koodi = substring(Alue, 3,5),
-                     seutukunta = substring(Alue, 7)) %>%
-              select(-Alue, -seutukunta_koodi)
+              mutate(seutukunta_code = factor(statfitools::extract_code(Alue, numbers_as_numeric = FALSE)),
+                     seutukunta_name = factor(statfitools::extract_name(Alue))) %>%
+              select(-Alue)
   } else if (region == "maakunta") {
     output <- filter(data, grepl("MK", Alue)) %>%
-              mutate(maakunta_koodi = substring(Alue, 3,4),
-                     maakunta = substring(Alue, 6)) %>%
-              select(-Alue, -maakunta_koodi)
+              mutate(maakunta_code = factor(statfitools::extract_code(Alue, numbers_as_numeric = FALSE)),
+                     maakunta_name = factor(statfitools::extract_name(Alue))) %>%
+              select(-Alue)
   } else if (region %in% c("koko maa", "KOKO MAA")) {
       output <- filter(data, grepl("KOKO MAA", Alue)) %>%
                 select(-Alue)
